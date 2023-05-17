65586d0204bbefa69bc2074050a4a93631c373ed
bhalex <133658851+bhalex@users.noreply.github.com>
2023-05-17 14:42:55 +0300

    Update 07_task_tuple.py

--- Module4/practice/02_list_and_tuple/07_task_tuple.py
+++ Module4/practice/02_list_and_tuple/07_task_tuple.py
@@ -1,5 +1,10 @@
 # Дан кортеж заполненный целыми числами
 # Найдите самый большой элемент кортежа
+
 tup = (2, 4, 6, -4, 12, 0, 5)
 
-# TODO: your code here
+my_list = list(tup)
+my_list.sort()
+my_list.reverse()
+
+print(my_list[0])
